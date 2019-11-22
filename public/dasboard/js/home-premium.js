5867fac165f5e0af59286c67ea9670ef154d641b
Handifajars <42840663+Handifajars@users.noreply.github.com>
2019-11-22 09:18:13 +0700

    Add files via upload

---
+++ public/dasboard/js/home-premium.js
@@ -0,0 +1,2 @@
+$(function(){Messenger.options={extraClasses:"messenger-fixed messenger-on-top  messenger-on-right",
+	theme:"flat",messageDefaults:{showCloseButton:!0}},Messenger().post({message:"Haloo, Selamat Datang Di Sistem Pengajuan PDAM",type:"success"})});
